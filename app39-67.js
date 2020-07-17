e83a9ee9439b996f0110282d32be148fa8ab608c
aqasim23 <57294858+aqasim23@users.noreply.github.com>
2020-07-18 02:00:17 +0500

    added all assignments chapters 1-67

---
+++ app39-67.js
@@ -0,0 +1,46 @@
+// Chapter 38 to 42
+
+// Task 1
+
+// function pow(a,b) {
+//     return (a**b);
+// }
+// var a = +prompt("Enter number");
+// var b = +prompt("Enter power");
+// document.write(pow(a,b));
+
+
+// Task 2
+
+// function isLeap(year){
+//     if ((year%4) == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// var yr = +prompt("Enter Year");
+// if (isLeap(yr)) {
+//     document.write(yr + " is a leap year");
+// } else {
+//     document.write(yr + " is not a leap year");
+// }
+
+
+// Taks 3
+
+// function area(a,b,c){
+//     function S(a,b,c) {
+//         return ((a+b+c)/2);
+//     }
+//     var s2 = S(a,b,c);
+//     return (s2*(s2-a)*(s2-b)*(s2-c))**(0.5);
+// }
+// var inpA = +prompt("Enter value for side a");
+// var inpB = +prompt("Enter value for side b");
+// var inpC = +prompt("Enter value for side c");
+// document.write("Area for the given triangle is " + area(inpA,inpB,inpC));
+
+
+// Task 4
+
